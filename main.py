e51d028dc5d56b646080658cf3758837b122d737
Tylo <132717358+tlelv@users.noreply.github.com>
2024-02-12 17:41:20 -0700

    Complete

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
+import os
+import shutil
+import time
+import logging
+from os.path import exists, splitext
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+source_dir = "/Users/treysonle/Downloads"
+dest_dir_sfx = "/Users/treysonle/Desktop/Sound"
+dest_dir_music = "/Users/treysonle/Desktop/Music"
+dest_dir_video = "/Users/treysonle/Desktop/Downloaded Videos"
+dest_dir_image = "/Users/treysonle/Desktop/Downloaded Images"
+
+def makeUnique(dest, name):
+    filename, extension = splitext(name)
+    counter = 1
+    # If file exists, adds a number to the end of the filename
+    while exists(f"{dest}/{name}"):
+        name = f"{filename}({str(counter)}){extension}"
+        counter += 1
+
+    return name
+
+def move(dest, entry, name):
+    file_exists = os.path.exists(dest + "/" + name)
+    if file_exists:
+        unique_name = makeUnique(dest, name)
+        os.rename(entry, unique_name)
+    shutil.move(entry, dest)
+
+class MoverHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        with os.scandir(source_dir) as entries:
+            for entry in entries:  #Loops all the files in the folder
+                name =  entry.name
+                dest = source_dir
+                if name.endswith('.wav') or name.endswith('.mp3'):
+                    if entry.stat().st_size < 25000000 or "SFX" in name:
+                        dest = dest_dir_sfx
+                    else:
+                        dest = dest_dir_music
+                    move(dest, entry, name)
+                elif name.endswith('.mov') or name.endswith('.mp4'): #If file is a vide
+                    dest = dest_dir_video
+                    move(dest, entry, name)
+                elif name.endswith('.jpg') or name.endswith('.jpeg') or name.endswith('.png'):
+                    dest = dest_dir_image
+                    move(dest, entry, name)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    path = source_dir
+    event_handler = MoverHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(10)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
